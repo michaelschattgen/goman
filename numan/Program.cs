4d695216acc9be9cc24cb48be7b258e73c0cfd92
Michael Schättgen <michael@schattgen.me>
2025-02-19 01:10:20 +0100

    Initial commit

---
+++ numan/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.CommandLine;
+using Spectre.Console;
+
+var rootCommand = new RootCommand("numan - NuGet Package Manager");
+
+var updateCommand = new Command("test", "Test if this new cli tool works properly");
+updateCommand.SetHandler(() =>
+{
+    AnsiConsole.MarkupLine("[green]it works 😎[/]");
+});
+
+rootCommand.AddCommand(updateCommand);
+rootCommand.Invoke(args);
